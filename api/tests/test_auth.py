5d0bc53abb7e6239930346f5556f0af37c4eb5e6
Adebowale Adam <adebowaleadam0@gmail.com>
2026-01-22 00:30:18 +0100

    test for profile

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -34,6 +34,40 @@ def test_user_can_login_and_receive_token():
         format='json'
     )
     assert response.status_code ==200
-    assert 'access' in response.data
+    assert 'access' in response.dataVerify whether a user is authenticated
+
+Fetch the current user’s identity
+
+Bootstrap user-specific data (dashboard, preferences, etc.)
 
 
+
+@pytest.mark.django_db
+
+def test_authenticated_user_can_access_profile_endpoint():
+    user = User.objects.create_user(
+        username="john",
+        password='password',
+        email='jogndoe@example.com'
+    )
+    refresh = RefreshToken.for_user(user)
+
+    client = APIClient()
+
+    client.credentials(
+        HTTP_AUTHORIZATION= f'Bearer   {refresh.access_token}'
+    )
+
+    response = client.get("/api/auth/me/")
+
+    assert response.status_code == 200
+    assert response.data['username'] == "john"
+    assert response.email['email'] =='johndoe@example.com'
+
+
+@pytest.mark.django_dbclient = APIClient()
+def test_unauthurised_user_cannot_access_profile_endpoint():
+    client = APIClient()
+    response = client.get('/api/auth/me/')
+
+    assert response.status_code == 404
